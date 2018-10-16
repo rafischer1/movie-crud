620b1b16ee7b019fc51dbd7bd7e1d8e5ea4c775f
Robert Fischer <artiefischer@gmail.com>
2018-10-16 16:41:32 -0600

    production

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,7 @@ module.exports = {
   test: {},
   production: {
     client: 'pg',
-    connection: process.env.postgres: //ocfmpkflmgujav:7816910621ab087eba079f9e5167c39e78856bb251474d0d3decd4e18ae3a4d7@ec2-184-72-234-230.compute-1.amazonaws.com:5432/d13g2sisjcmhdo
+    connection: postgres: //ocfmpkflmgujav:7816910621ab087eba079f9e5167c39e78856bb251474d0d3decd4e18ae3a4d7@ec2-184-72-234-230.compute-1.amazonaws.com:5432/d13g2sisjcmhdo,
+
   }
 }
