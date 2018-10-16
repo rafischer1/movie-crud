4133bba8eaecd4cb7fb30718556eee133b5c8a60
Robert Fischer <artiefischer@gmail.com>
2018-10-16 17:44:30 -0600

    cors/

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const actorsRouter = require('./routes/actors');
 
 var app = express();
 
+// Allow CORS.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -30,13 +37,8 @@ app.use('/actors', actorsRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-//cors 
-// Allow CORS.
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
