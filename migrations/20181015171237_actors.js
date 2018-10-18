7f93ef1dcdbc17c56e3704a665a57e7281c3b9b4
Robert Fischer <artiefischer@gmail.com>
2018-10-18 11:58:21 -0600

    photo

--- migrations/20181015171237_actors.js
+++ migrations/20181015171237_actors.js
@@ -2,7 +2,6 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('actors', table => {
     table.increments('id').primary().notNullable()
     table.string('name').notNullable().defaultTo('')
-    table.integer('movies_id').unsigned().references('movies.id').notNullable().onDelete('cascade')
     table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
     table.timestamp('updated_at').defaultTo(knex.fn.now()).notNullable()
   })
--- seeds/02_actors_seeds.js
+++ seeds/02_actors_seeds.js
@@ -4,17 +4,7 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       // Inserts seed entries
       return knex('actors').insert([
-        { id: 1, name: "Klaus Kinski", movies_id: 2 },
-        { id: 2, name: "Christian Bale", movies_id: 1 },
-        { id: 3, name: "Isabelle Adjani", movies_id: 4 },
-        { id: 4, name: "Nicholas Cage", movies_id: 6 },
-        { id: 5, name: "Bruno S", movies_id: 5 },
-        { id: 6, name: "Josef Bierbichler", movies_id: 8 },
-        { id: 7, name: "Klaus Kinski", movies_id: 3 },
-        { id: 8, name: "Klaus Kinski", movies_id: 9 },
-        { id: 9, name: "Bruno S", movies_id: 7 },
-        { id: 10, name: "Walter Ladengast", movies_id: 5 },
-        { id: 11, name: "Steve Zahn", movies_id: 1 },
+        {}
       ]);
     })
     .then(() => {
